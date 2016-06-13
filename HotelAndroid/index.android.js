3e2b82ceb7fa7aa6f8e3bc34dba52501ff6f81e7
Minjeong Kim <min7859@gmail.com>
2016-06-13 23:13:46 +0900

    (feat) Added date picker in check-in/check-out
    reference: https://facebook.github.io/react-native/docs/datepickerandroid.html

--- HotelAndroid/index.android.js
+++ HotelAndroid/index.android.js
@@ -11,6 +11,8 @@ import {
   Text,
   View,
   ListView,
+  DatePickerAndroid,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import Button from 'react-native-button';
 import DropDown, {
@@ -20,13 +22,12 @@ import DropDown, {
 } from 'react-native-selectme';
 
 class HotelAndroid extends Component {
-  static defaultProps = {
-    ds: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-  }
-
   state = {
-    dataSource: this.props.ds.cloneWithRows(['지역', '체크인', '체크아웃', '객실수', '투숙객']),
-    location: ''
+    location: '',
+    checkinDate: new Date(),
+    checkinText: '체크인 날짜 선택',
+    checkoutDate: new Date(),
+    checkoutText: '체크아웃 날짜 선택',
   }
 
   _getOptionList() {
@@ -65,6 +66,23 @@ class HotelAndroid extends Component {
     console.log('pressed!');
   }
 
+  async showPicker(stateKey, options) {
+    try {
+      var newState = {};
+      const {action, year, month, day} = await DatePickerAndroid.open(options);
+      if (action === DatePickerAndroid.dismissedAction) {
+        newState[stateKey + 'Text'] = 'dismissed';
+      } else {
+        var date = new Date(year, month, day);
+        newState[stateKey + 'Text'] = date.toLocaleDateString();
+        newState[stateKey + 'Date'] = date;
+      }
+      this.setState(newState);
+    } catch ({code, message}) {
+      console.warn(`Error in example '${stateKey}': `, message);
+    }
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
@@ -79,7 +97,7 @@ class HotelAndroid extends Component {
               width={250}
               ref="SELECT1"
               optionListRef={this._getOptionList.bind(this)}
-              defaultValue="지역을 선택해주세요"
+              defaultValue="지역을 선택"
               onSelect={this._location.bind(this)}>
               <Option>서울</Option>
               <Option>대전</Option>
@@ -89,12 +107,41 @@ class HotelAndroid extends Component {
           </View>
 
           <View style={styles.rowContainer}>
+            <Text style={styles.label}>체크인</Text>
+            <TouchableWithoutFeedback
+                onPress={this.showPicker.bind(this, 'checkin', {
+                  date: this.state.checkinDate,
+                  minDate: this.state.checkinDate,
+                  maxDate: new Date().setDate(this.state.checkinDate.getDate() + 14),
+                })}>
+              <View>
+                <Text style={styles.text}>{this.state.checkinText}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+
+          <View style={styles.rowContainer}>
+            <Text style={styles.label}>체크아웃</Text>
+            <TouchableWithoutFeedback
+                onPress={this.showPicker.bind(this, 'checkout', {
+                  date: this.state.checkoutDate,
+                  minDate: this.state.checkinDate,
+                  maxDate: new Date().setDate(this.state.checkinDate.getDate() + 7),
+                })}>
+              <View>
+                <Text style={styles.text}>{this.state.checkoutText}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+
+          </View>
+
+          <View style={styles.rowContainer}>
             <Text style={styles.label}>객실수</Text>
             <Select
               width={250}
               ref="SELECT2"
               optionListRef={this._getOptionList.bind(this)}
-              defaultValue="객실수를 선택해주세요"
+              defaultValue="객실수를 선택"
               onSelect={this._roomNumber.bind(this)}>
               <Option>1</Option>
               <Option>2</Option>
@@ -105,10 +152,10 @@ class HotelAndroid extends Component {
           <View style={styles.rowContainer}>
             <Text style={styles.label}>투숙객</Text>
             <Select
-              width={50}
+              width={70}
               ref="SELECT3"
               optionListRef={this._getOptionList.bind(this)}
-              defaultValue="성인수를 선택해주세요"
+              defaultValue="성인수"
               onSelect={this._adult.bind(this)}>
               <Option>1</Option>
               <Option>2</Option>
@@ -116,10 +163,10 @@ class HotelAndroid extends Component {
             </Select>
 
             <Select
-              width={50}
+              width={70}
               ref="SELECT4"
               optionListRef={this._getOptionList.bind(this)}
-              defaultValue="아동수를 선택해주세요"
+              defaultValue="아동수"
               onSelect={this._child.bind(this)}>
               <Option>1</Option>
               <Option>2</Option>
@@ -156,7 +203,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   label: {
-    width: 50,
+    width: 60,
     textAlign: 'left',
     margin: 10,
   },
