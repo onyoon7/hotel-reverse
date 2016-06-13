38f0ad185e8479fd09ea9b1887742b86b9fc470f
CHOSANG WOOK <chosangwook@CHOSANGui-MacBook-Pro.local>
2016-06-13 13:33:37 +0900

    (feat) Wrote 1st version of webpack.config.js
    Copied webpack.config.js from react exercise project.
    Modification required

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,4 +1,6 @@
-var webpack = require('webpack'); var path = require('path');
+var webpack = require('webpack'); 
+var path = require('path');
+
 module.exports = {
   devtool: 'inline-source-map', entry: [
     'webpack-dev-server/client?http://127.0.0.1:8080/', 'webpack/hot/only-dev-server',
