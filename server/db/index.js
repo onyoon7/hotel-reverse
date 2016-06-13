b7e6b9c462bb68b44a2ad3a0da68a6610dec08ea
CHOSANG WOOK <chosangwook@CHOSANGui-MacBook-Pro.local>
2016-06-13 13:28:58 +0900

    (feat) Connect to DB(hotelreverse)
    Set up mysql connection, database name is hotelreverse

---
+++ server/db/index.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+/*--------------------------------------------------------------------
+  1. Create a MySQL database connection and export it
+  - host: localhost
+  - user: root
+  - password: (root password of individual installation,
+               in my case: lion0787)
+  - database: hotel-reverse (let's determine database name)
+
+  2. Then connect to 'hotel-reverse' database
+ --------------------------------------------------------------------*/
+
+module.exports.connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'lion0787',
+  database: 'hotelreverse'
+});
+
+
+
+module.exports.connection.connect(function(err) {
+  
+  if (err) {
+    console.log('error connecting...');
+    return;
+  }
+
+  console.log('successfully connected to database...');
+
+});
