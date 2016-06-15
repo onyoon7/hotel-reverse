a66d9b105a81a9db018866807862e66eb2ddd506
Minjeong Kim <min7859@gmail.com>
2016-06-15 18:04:38 +0900

    (feat) Configured watching server files
    added file cache in babel
    used watch in nodemon

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -5,21 +5,24 @@ const babel = require('gulp-babel');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
 const nodemon = require('gulp-nodemon');
+const Cache = require('gulp-file-cache');
+const cache = new Cache();
 const paths = {
-  eslint: ['./**/*.js', '!public/**', '!node_modules/**'],
-  babel: ['./**/*.js', '!public/**', '!admin/**', '!manager/**', '!node_modules/**'],
+  server: ['./src/**/*.js'],
 };
 
 gulp.task('babel', () =>
-  gulp.src(paths.babel)
+  gulp.src(paths.server)
+  .pipe(cache.filter())
   .pipe(babel({
     presets: ['es2015']
   }))
-  .pipe(gulp.dest('babel'))
+  .pipe(cache.cache())
+  .pipe(gulp.dest('build'))
 );
 
 gulp.task('eslint',() =>
-  gulp.src(paths.eslint)
+  gulp.src(paths.server)
   .pipe(eslint({
     'parserOptions' : {
       'ecmaVersion' : 6,
@@ -34,20 +37,16 @@ gulp.task('eslint',() =>
   .pipe(eslint.format())
 );
 
-gulp.task('watch', () => {
-  gulp.watch(paths.eslint, ['eslint']);
-  gulp.watch(paths.babel, ['babel']);
-});
-
 gulp.task('webpack', () =>
   gulp.src('').pipe(webpack(require('./webpack.config.js')))
 );
 
-gulp.task('serve', () => {
+gulp.task('serve', ['babel'], () => {
   nodemon({
-    script: 'babel/server.js',
-    ignore: 'node_modules/**'
+    script: 'build/server.js',
+    watch: 'src',
+    tasks: ['babel']
   });
 });
 
-gulp.task('default', ['babel', 'webpack', 'eslint', 'serve', 'watch']);
+gulp.task('default', ['babel', 'webpack', 'serve']);
