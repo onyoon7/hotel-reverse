bb3d7984a6c19c9ca201b2485ea6531f021efb0d
Byungjune Yoon <on.yoon7@gmail.com>
2016-06-16 19:48:00 +0900

    (feat)Added the client page - bid, login, register
    Special thanks to Nyeon.
    1. Bid to enter the Subarea, price
    2. Login to check login or skip it
    3. Register to be the member

--- HotelAndroid/index.android.js
+++ HotelAndroid/index.android.js
@@ -13,18 +13,64 @@ import {
 
 import HotelSearch from './hotelSearch';
 import HotelBid from './hotelBid';
+import HotelSignin from './hotelSignin';
+import Register from './register';
+
 
 let _navigator;
 
 class HotelAndroid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location : '',
+      client_email : '',
+      checkinText : '체크인 날짜 선택',
+      checkoutText : '체크아웃 날짜 선택',
+      roomNumber: 1,
+      hotel_Rate : 3,
+      hotel_SubArea : '강남',
+      bidding_Price : 35000,
+    };
+    this.stateChagned = this.stateChagned.bind(this);
+    this.navigatorRenderScene = this.navigatorRenderScene.bind(this);
+  }
+
+  searchStateChagned(location, checkinText, checkoutText, roomNumber) {
+    this.setState({
+      location : location,
+      checkinText : checkinText,
+      checkoutText : checkoutText,
+      roomNumber: roomNumber,
+    });
+  }
+
+  bidStateChagned(hotel_Rate, hotel_SubArea, bidding_Price) {
+    this.setState({
+      hotel_Rate : hotel_Rate,
+      hotel_SubArea : hotel_SubArea,
+      bidding_Price : bidding_Price
+    });
+  }
+
+  signinStateChagned(client_email) {
+    this.setState({
+      client_email : client_email,
+    });
+  }
 
   navigatorRenderScene(route, navigator) {
     _navigator = navigator;
     switch (route.id) {
       case 'search':
-        return (<HotelSearch navigator={navigator}/>);
+        return (<HotelSearch navigator={navigator} onChange={this.searchStateChagned}/>);
       case 'bid':
-        return (<HotelBid navigator={navigator}/>);
+        return (<HotelBid navigator={navigator} onChange={this.bidStateChagned}/>);
+      case 'signin':
+        return (<HotelSignin navigator={navigator} onChange={this.signinStateChagned}/>);
+      case 'register':
+        return (<Register navigator={navigator} location={this.state.location} {...route.passPorps}/>);
     }
   }
 
@@ -37,6 +83,7 @@ class HotelAndroid extends Component {
   }
 }
 
+//https://facebook.github.io/react-native/docs/backandroid.html
 BackAndroid.addEventListener('hardwareBackPress', () => {
   if (_navigator.getCurrentRoutes().length === 1) {
     return false;
