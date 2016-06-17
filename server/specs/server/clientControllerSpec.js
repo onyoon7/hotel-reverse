c0ec7595a60c7a78a9795434068e7add105f4654
Minjeong Kim <min7859@gmail.com>
2016-06-17 14:08:11 +0900

    (test) Added admin/pendingbid mocha test

--- server/specs/server/clientControllerSpec.js
+++ server/specs/server/clientControllerSpec.js
@@ -1,13 +1,11 @@
-var request = require('supertest');
+var supertest = require('supertest');
 var should = require('should');
-var app = require('../../src/server.js');
+let server = supertest.agent('http://localhost:4444');
 
 describe('Server RestAPI unit test', function() {
   it('should GET admin pendingbid', function(done) {
-    request(app)
+    server
       .get('/admin/pendingbid')
-      .expect('Content-Type', /json/)
-      .expect('Content-Length', '15')
       .expect(200)
       .end(function(err, res) {
         if (err) throw err;
