9a65db3e879c661c5865c62c9a9c25e0c1b6d7e8
Minjeong Kim <min7859@gmail.com>
2016-06-22 15:35:40 +0900

    (refactor) Moduled coordinates vars and eslinted code
    eslint config is smame in gulpfile except semicolon rule

--- HotelAndroid/hotelBid.js
+++ HotelAndroid/hotelBid.js
@@ -2,233 +2,159 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  TextInput,
   View,
-  TouchableWithoutFeedback,
   Picker,
-  Navigator,
   Dimensions,
   Slider,
 } from 'react-native';
 const Item = Picker.Item;
 import Button from 'react-native-button';
 import MapView from 'react-native-maps';
+import coordinates from './assets/coordinates';
 
-let { width, height } = Dimensions.get('window');
-
-const ASPECT_RATIO = width / height / 2;
-const LATITUDE_DELTA = 0.1522;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const { width } = Dimensions.get('window');
 
 class HotelBid extends Component {
   constructor(props){
     super(props)
 
-    let seoul = [
-      // gangnam-gu
-      { key: 'gangnam', value: [
-          {
-            latitude: 37.533335,
-            longitude: 127.027770,
-          },
-          {
-            latitude: 37.503381,
-            longitude: 127.069827,
-          },
-          {
-            latitude: 37.468237,
-            longitude: 127.122012,
-          },
-          {
-            latitude: 37.456995,
-            longitude: 127.098164,
-          },
-          {
-            latitude: 37.469115,
-            longitude: 127.053685,
-          },
-        ],
-      },
-      // myeong-dong
-      { key: 'myeongdong', value: [
-          {
-            latitude: 37.568933,
-            longitude: 126.976930,
-          },
-          {
-            latitude: 37.567810,
-            longitude: 126.989075,
-          },
-          {
-            latitude: 37.556414,
-            longitude: 126.985041,
-          },
-          {
-            latitude: 37.563592,
-            longitude: 126.982080,
-          },
-        ],
-      },
-      { key: 'yeouido', value: [
-          {
-            latitude: 37.541101,
-            longitude: 126.927290,
-          },
-          {
-            latitude: 37.518230,
-            longitude: 126.940766,
-          },
-          {
-            latitude: 37.520273,
-            longitude: 126.916132,
-          },
-          {
-            latitude: 37.532865,
-            longitude: 126.911755,
-          },
-        ],
-      },
-    ];
-
-    let jeju = [];
-
+    let seoul = coordinates.seoul;
+    let jeju = coordinates.jeju;
+    let seoulPolygons = coordinates.seoulPolygons;
+    let jejuPolygons = coordinates.jejuPolygons;
     function getPolygonCmpt(region) {
       return region.map(item => {
         let r = Math.floor(Math.random()*255), g = Math.floor(Math.random()*255), b = Math.floor(Math.random()*255);
         const fillColor = `rgba(${r},${g},${b},0.5)`;
-          return (
-            <MapView.Polygon
-              key={item.key}
-              coordinates={item.value}
-              fillColor={fillColor}
-              strokeColor="rgba(0,0,0,0.5)"
-              stokeWidth={2}
-            />
-          )
-        }
+        return (
+          <MapView.Polygon
+          key={item.key}
+          coordinates={item.value}
+          fillColor={fillColor}
+          strokeColor="rgba(0,0,0,0.5)"
+          stokeWidth={2}
+          />
+        )
+      }
       );
     }
 
-    this.state={
+    this.region = {
+      seoul: seoul,
+      jeju: jeju,
+    }
+
+    this.polygon = {
+      seoul: getPolygonCmpt(seoulPolygons),
+      jeju: getPolygonCmpt(jejuPolygons),
+    }
+
+    this.state = {
       subArea_Name: '',
       hotel_Rate: 5,
       bid_Price: '80000',
-      region : {
-        seoul: {
-          latitude: 37.552547,
-          longitude: 126.993552,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        },
-        jeju: {
-        },
-      },
-      polygon: {
-        seoul: getPolygonCmpt(seoul),
-        jeju: getPolygonCmpt(jeju),
-      },
     }
-   }
+  }
 
-   _handlePress() {
-     this.props.navigator.push({id: 'signin'});
-     this.props.onChange(this.state.hotel_Rate, this.state.subArea_Name, this.state.bid_Price);
-   }
+  _handlePress() {
+    this.props.navigator.push({id: 'signin'});
+    this.props.onChange(this.state.hotel_Rate, this.state.subArea_Name, this.state.bid_Price);
+  }
 
-   onValueChange(key: string, value: string) {
-     const newState = {};
-     newState[key] = value;
-     this.setState(newState);
-   }
+  onValueChange(key: string, value: string) {
+    const newState = {};
+    newState[key] = value;
+    this.setState(newState);
+  }
 
-   render() {
-     let region;
-     let polygonName;
+  render() {
+    let region;
+    let polygonName;
 
-     const seoulArea = ['강남구', '서초구', '명동', '여의도'];
-     const jejuArea = ['제주시', '서귀포시'];
+    const seoulArea = ['강남구', '서초구', '명동', '여의도'];
+    const jejuArea = ['제주시', '서귀포시'];
 
-     const rows = [];
-     const mainArea = this.props.searchData.mainArea_Name;
+    const rows = [];
+    const mainArea = this.props.searchData.mainArea_Name;
 
-     if(mainArea === '서울'){
-       for(const i = 0; i < seoulArea.length; i++){
-         rows.push(<Item label={seoulArea[i]} key={i} value={seoulArea[i]} />);
-       }
-       region = this.state.region.seoul;
-       polygonName = 'seoul';
-     } else if(mainArea === '제주'){
-       for(const i = 0; i < jejuArea.length; i++){
-         rows.push(<Item label={jejuArea[i]} key={i} value={jejuArea[i]} />);
-       }
-       region = this.state.region.jeju;
-       polygonName = 'jeju';
-     }
+    if(mainArea === '서울'){
+      for(let i = 0; i < seoulArea.length; i++){
+        rows.push(<Item label={seoulArea[i]} key={i} value={seoulArea[i]} />);
+      }
+      region = this.state.region.seoul;
+      polygonName = 'seoul';
+    } else if(mainArea === '제주'){
+      for(let i = 0; i < jejuArea.length; i++){
+        rows.push(<Item label={jejuArea[i]} key={i} value={jejuArea[i]} />);
+      }
+      region = this.state.region.jeju;
+      polygonName = 'jeju';
+    }
 
-     return (
-       <View style={styles.container}>
-         <View style={styles.rowContainer}>
-           <Text style={styles.title}>
-             {this.props.searchData.mainArea_Name}의 어디에서 묵고 싶나요?
-           </Text>
-         </View>
+    return (
+      <View style={styles.container}>
+        <View style={styles.rowContainer}>
+          <Text style={styles.title}>
+            {this.props.searchData.mainArea_Name}의 어디에서 묵고 싶나요?
+          </Text>
+        </View>
 
-         <MapView
-           style={{width: width, flex: 1}}
-           initialRegion={region}
-         >
-           {this.state.polygon[polygonName]}
-         </MapView>
+        <MapView
+          style={{width: width, flex: 1}}
+          initialRegion={region}
+        >
+          {this.state.polygon[polygonName]}
+        </MapView>
 
-         <View style={{flex: 1}}>
-           <View style={styles.rowContainer}>
-             <Text style={styles.label}>세부지역</Text>
-             <Picker style={{width: 150}}
-               selectedValue={this.state.subArea_Name}
-               onValueChange={this.onValueChange.bind(this, 'subArea_Name')}
-               mode="dropdown">
-               {rows}
-             </Picker>
-           </View>
+        <View style={{flex: 1}}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.label}>세부지역</Text>
+            <Picker style={{width: 150}}
+              selectedValue={this.state.subArea_Name}
+              onValueChange={this.onValueChange.bind(this, 'subArea_Name')}
+              mode="dropdown">
+              {rows}
+            </Picker>
+          </View>
 
-           <View style={styles.rowContainer}>
-             <Text style={styles.label}>호텔등급</Text>
-             <Picker style={{width: 150}}
-               selectedValue={this.state.hotel_Rate}
-               onValueChange={this.onValueChange.bind(this, 'hotel_Rate')}
-               mode="dropdown">
-               <Item label="★★★★★" value="5" />
-               <Item label="★★★★" value="4" />
-               <Item label="★★★" value="3" />
-             </Picker>
-           </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.label}>호텔등급</Text>
+            <Picker style={{width: 150}}
+              selectedValue={this.state.hotel_Rate}
+              onValueChange={this.onValueChange.bind(this, 'hotel_Rate')}
+              mode="dropdown">
+                <Item label="★★★★★" value="5" />
+                <Item label="★★★★" value="4" />
+                <Item label="★★★" value="3" />
+            </Picker>
+          </View>
 
-           <View style={styles.rowContainer}>
-             <Text style={styles.price}>
-               ₩ {this.state.bid_Price}
-             </Text>
-           </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.price}>
+              ₩ {this.state.bid_Price}
+            </Text>
+          </View>
 
-           <Slider
+          <Slider
             minimumValue={40000}
             maximumValue={150000}
             value={80000}
             step={1000}
             onValueChange={(value) => this.setState({bid_Price: value})}
-           />
+          />
 
-           <View style={styles.rowContainer}>
-             <Button
-               containerStyle={styles.button}
-               style={styles.buttonText}
-               onPress={() => this._handlePress()}>
-               계속하기
-             </Button>
-           </View>
-         </View>
-       </View>
-     );
-   }
+          <View style={styles.rowContainer}>
+            <Button
+              containerStyle={styles.button}
+              style={styles.buttonText}
+              onPress={() => this._handlePress()}>
+                계속하기
+            </Button>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -242,45 +168,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   rowContainer: {
-   flexDirection: 'row',
-   justifyContent: 'center',
-   alignItems: 'center',
-   margin: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
   },
   title: {
-   fontSize: 20,
-   textAlign: 'center',
-   color: '#000',
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#000',
   },
   label: {
-   width: 60,
-   textAlign: 'left',
-   margin: 10,
-   color: 'black',
+    width: 60,
+    textAlign: 'left',
+    margin: 10,
+    color: 'black',
   },
   button: {
-   width: 150,
-   padding: 10,
-   height: 30,
-   overflow: 'hidden',
-   borderColor: 'black',
-   borderWidth: 2,
-   borderStyle: 'solid',
-   backgroundColor: 'green',
-   justifyContent: 'center',
-   alignItems: 'center',
-   margin: 30,
+    width: 150,
+    padding: 10,
+    height: 30,
+    overflow: 'hidden',
+    borderColor: 'black',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 30,
   },
   buttonText: {
-   fontSize: 15,
-   color: 'white',
+    fontSize: 15,
+    color: 'white',
   },
   price: {
     fontSize: 18,
     color: 'black',
   },
 });
-
-
 
 export default HotelBid;
