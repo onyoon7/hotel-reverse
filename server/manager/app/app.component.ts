f5cfa8bc04e985aeeadb2243ab6d3dd8d82433ae
NyeonKim <nyeonkim91@gmail.com>
2016-06-24 18:29:37 +0900

    (feat) Created signin page in manager side
    - set routes in app.component.ts, boot.ts
    - created signin page
    - added form to post id&pw and connected to server

--- server/manager/app/app.component.ts
+++ server/manager/app/app.component.ts
@@ -3,14 +3,23 @@ import {GetService} from './get.service';
 import {PostService} from './post.service';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {OnInit} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {hotelSignin} from './hotel_signin.component';
 
 @Component({
     selector: 'my-app',
     template: `
+    <router-outlet></router-outlet>
     `,
-    providers: [GetService, PostService, HTTP_PROVIDERS]
+    providers: [GetService, PostService, HTTP_PROVIDERS],
+    directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent implements OnInit { 
+
+@RouteConfig([
+  {path: '/hotel/signin', component: hotelSignin, name: 'HotelSignin'}
+])
+
+export class AppComponent implements OnInit {
 
     constructor(private _getService: GetService, private _postService: PostService) {
 
--- server/manager/app/post.service.ts
+++ server/manager/app/post.service.ts
@@ -1,4 +1,4 @@
-import {Http} from 'angular2/http';
+import {Http, Headers, HTTP_PROVIDERS} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {Injectable} from 'angular2/core';
@@ -14,7 +14,14 @@ export class PostService {
   }
 
   hotelSignIn(hotel) {
-    return this._http.post(this._url_hotel_signin, JSON.stringify(hotel))
+    var hotel_ID = hotel.hotel_ID;
+    var hotel_PW = hotel.hotel_PW;
+
+    var hotel_Info = "hotel_ID=" + hotel_ID + "&hotel_PW=" + hotel_PW;
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    return this._http.post(this._url_hotel_signin, hotel_Info, {headers: headers})
         .map(res => res.json());
   }
 
