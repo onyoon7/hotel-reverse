f7fc613d065a732d06a9b7f9fad849e5e8b82d36
Minjeong Kim <min7859@gmail.com>
2016-06-27 13:52:24 +0900

    (feat) Added jeju polygons
    제주시, 서귀포시

--- HotelAndroid/assets/areaInfo.js
+++ HotelAndroid/assets/areaInfo.js
@@ -5,11 +5,13 @@ const ASPECT_RATIO = width / height / 2;
 const LATITUDE_DELTA = 0.1522;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+const area = {
+  seoul: ['강남구', '서초구', '명동', '여의도'],
+  jeju: ['제주시', '서귀포시'],
+}
+
 export default {
-  area: {
-    seoul: ['강남구', '서초구', '명동', '여의도'],
-    jeju: ['제주시', '서귀포시'],
-  },
+  area: area,
   region: {
     seoul: {
       latitude: 37.552547,
@@ -18,12 +20,16 @@ export default {
       longitudeDelta: LONGITUDE_DELTA,
     },
     jeju: {
+      latitude: 33.499634,
+      longitude: 126.531223,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
     },
   },
   polygon: {
     seoul: [
       // gangnam-gu
-      { key: 'gangnam', value: [
+      { key: area.seoul[0], value: [
         {
           latitude: 37.533335,
           longitude: 127.027770,
@@ -47,7 +53,7 @@ export default {
       ],
       },
       // myeong-dong
-      { key: 'myeongdong', value: [
+      { key: area.seoul[2], value: [
         {
           latitude: 37.568933,
           longitude: 126.976930,
@@ -66,7 +72,7 @@ export default {
         },
       ],
       },
-      { key: 'yeouido', value: [
+      { key: area.seoul[3], value: [
         {
           latitude: 37.541101,
           longitude: 126.927290,
@@ -86,6 +92,45 @@ export default {
       ],
       },
     ],
-    jeju: [],
+    jeju: [
+      { key: area.jeju[0], value: [
+        {
+          latitude: 33.516733,
+          longitude: 126.524663,
+        },
+        {
+          latitude: 33.499781,
+          longitude: 126.511951
+        },
+        {
+          latitude: 33.487112,
+          longitude: 126.534887
+        },
+        {
+          latitude: 33.502141,
+          longitude: 126.549929,
+        },
+      ],
+      },
+      { key: area.jeju[1], value: [
+        {
+          latitude: 33.259158,
+          longitude: 126.561299
+        },
+        {
+          latitude: 33.253879,
+          longitude: 126.575245
+        },
+        {
+          latitude: 33.240068,
+          longitude: 126.563832
+        },
+        {
+          latitude: 33.253140,
+          longitude: 126.555264
+        },
+      ],
+      },
+    ],
   },
 };
