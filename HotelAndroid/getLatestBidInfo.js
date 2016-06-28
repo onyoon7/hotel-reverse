79a88172cdda1e53471771fad7df7b0d7beb8084
Minjeong Kim <min7859@gmail.com>
2016-06-28 14:33:49 +0900

    (feat) Improve TextInputs in bidInfo
    - Added Label
    - Set maxLength, returnKeyType, keyboardType, onSubmitEditing
    - Styled layouts

--- HotelAndroid/getLatestBidInfo.js
+++ HotelAndroid/getLatestBidInfo.js
@@ -16,7 +16,6 @@ const IMP_KEY = '3372420065794528';
 const IMP_SECRET = 'YwZIGQT4cEjESlJwSwrk4HadQE2QN4qLBpuhgnms2F7V1QrTmSdrAnEq2HhPLHBm76Enu0PwFXrGNTAa';
 const MERCHANT_UID = 'nictest14m';
 
-const alertMessage = '조심해라. 한번 체결되면 바로 돈 나간다!!!!'
 
 /*----------------------------------------------------------------
   Structure
@@ -29,12 +28,15 @@ class GetLatestBidInfo extends Component {
     super(props);
 
     this.state = {
-      card_number: '0000 0000 0000 0000 카드 번호',
-      expiry: 'YYYYMM 카드 만료일',
-      birth: 'YYMMDD 생년월일',
-      pwd_2digit: '카드 비밀번호 앞 두 자리',
+      card_number: '',
+      expiry: '',
+      birth: '',
+      pwd_2digit: '',
       client_Email: '',
     }
+
+    this.focusNextField = this.focusNextField.bind(this);
+    this.promptConfirmMsg = this.promptConfirmMsg.bind(this);
   }
 
   async _handlePress(where) {
@@ -92,88 +94,163 @@ class GetLatestBidInfo extends Component {
     }
   }
 
-
   onValueChange(key: string, value: string) {
     const newState = {};
     newState[key] = value;
     this.setState(newState);
   }
 
-
   async componentWillMount() {
     var email = await AsyncStorage.getItem('client_Email');
-    this.state.client_Email = email;
+    this.onValueChange('client_Email', email);
+  }
+  
+  focusNextField(nextField) {
+    this.refs[nextField].focus();
+  }
+
+  promptConfirmMsg() {
+    const alertMessage = '조심해라. 한번 체결되면 바로 돈 나간다!!!!'
+    Alert.alert('check', alertMessage, [
+        {text : 'Cancel', onPress: () => console.log('진행 취소')},
+        {text : 'OK', onPress: () => this._handlePress('thanks')}
+    ])
   }
 
   render() {
     return (
       <View style={{flex: 1}}>
-        <Text style={styles.appName}>
-          Your Wish List
-        </Text>
+        <View style={styles.rowContainer}>
+          <Text style={styles.title}>
+            Your Wish List
+          </Text>
+        </View>
 
-        <View style={styles.smallRowContainer}>
-          <Text>
-            {`-  고객 이메일: ${this.state.client_Email}`}
+        <View style={styles.rowContainer}>
+          <Text style={styles.category}>
+            고객 정보
           </Text>
         </View>
 
         <View style={styles.smallRowContainer}>
-          <Text>
-            {`-  희망 지역: ${this.props.searchData.mainArea_Name} ${this.props.bidData.subArea_Name}`}
+          <Text style={styles.label}>
+            고객 이메일&nbsp;:&nbsp;
+          </Text>
+          <Text style={styles.label}>
+            {this.state.client_Email}
           </Text>
         </View>
 
         <View style={styles.smallRowContainer}>
-          <Text>
-            {`-  체크인 날짜: ${this.props.searchData.checkIn_Date}`}
+          <Text style={styles.label}>
+            희망 지역&nbsp;:&nbsp;
+          </Text>
+          <Text style={styles.label}>
+            {`${this.props.searchData.mainArea_Name} ${this.props.bidData.subArea_Name}`}
           </Text>
         </View>
 
         <View style={styles.smallRowContainer}>
-          <Text>
-            {`-  체크아웃 날짜: ${this.props.searchData.checkOut_Date}`}
+          <Text style={styles.label}>
+            체크인 날짜&nbsp;:&nbsp;
+          </Text>
+          <Text style={styles.label}>
+             {this.props.searchData.checkIn_Date}
           </Text>
         </View>
 
         <View style={styles.smallRowContainer}>
-          <Text>
-            {`-  희망 가격: ${this.props.bidData.bid_Price}`}
+          <Text style={styles.label}>
+            체크아웃 날짜&nbsp;:&nbsp;
+          </Text>
+          <Text style={styles.label}>
+             {this.props.searchData.checkOut_Date}
           </Text>
         </View>
 
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => this.onValueChange('card_number', text)}
-          placeholder={this.state.card_number}
-        />
+        <View style={styles.smallRowContainer}>
+          <Text style={styles.label}>
+            희망 가격&nbsp;:&nbsp;
+          </Text>
+          <Text style={styles.label}>
+             {this.props.bidData.bid_Price}
+          </Text>
+        </View>
 
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => this.onValueChange('expiry', text)}
-          placeholder={this.state.expiry}
-        />
+        <View style={styles.rowContainer}>
+          <Text style={styles.category}>
+            카드 정보
+          </Text>
+        </View>
 
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => this.onValueChange('birth', text)}
-          placeholder={this.state.birth}
-        />
 
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => this.onValueChange('pwd_2digit', text)}
-          placeholder={this.state.pwd_2digit}
-        />
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>
+            번호
+          </Text>
+          <TextInput
+            ref="1"
+            style={[{width: 180}, styles.textInput]}
+            keyboardType="numeric"
+            returnKeyType="next"
+            maxLength={16}
+            onChangeText={(text) => this.onValueChange('card_number', text)}
+            placeholder="0000 0000 0000 0000"
+            onSubmitEditing={() => this.focusNextField('2')}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>
+            만료일
+          </Text>
+          <TextInput
+            ref="2"
+            style={[{width: 100}, styles.textInput]}
+            keyboardType="numeric"
+            returnKeyType="next"
+            maxLength={6}
+            onChangeText={(text) => this.onValueChange('expiry', text)}
+            placeholder="YYYYMM"
+            onSubmitEditing={() => this.focusNextField('3')}
+          />
+
+          <Text style={styles.label}>
+            비밀번호 앞 두 자리
+          </Text>
+          <TextInput
+            ref="3"
+            style={[{width: 50}, styles.textInput]}
+            keyboardType="numeric"
+            returnKeyType="next"
+            maxLength={2}
+            secureTextEntry={true}
+            onChangeText={(text) => this.onValueChange('pwd_2digit', text)}
+            placeholder="00"
+            onSubmitEditing={() => this.focusNextField('4')}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>
+            생년월일
+          </Text>
+          <TextInput
+            ref="4"
+            style={[{width: 80}, styles.textInput]}
+            keyboardType="numeric"
+            returnKeyType="done"
+            maxLength={6}
+            onChangeText={(text) => this.onValueChange('birth', text)}
+            placeholder="YYMMDD"
+            onSubmitEditing={this.promptConfirmMsg}
+          />
+        </View>
 
         <View style={styles.rowContainer}>
           <Button style={styles.searchBtnText}
             containerStyle={styles.searchBtn}
-            onPress={() => Alert.alert('check', alertMessage,
-              [
-                {text : 'Cancel', onPress: () => console.log('진행 취소')},
-                {text : 'OK', onPress: () => this._handlePress('thanks')}
-              ])}>
+            onPress={this.promptConfirmMsg}>
             신청하기
           </Button>
         </View>
@@ -205,20 +282,29 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     marginLeft: 30,
-    marginTop: 2,
-    marginBottom: 2
+    marginBottom: 8,
   },
-  appName: {
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginBottom: 5,
+  },
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    color: '#000',
-    margin: 10,
+    color: 'black',
   },
   label: {
-    width: 60,
+    textAlign: 'left',
+    color: 'black',
+    fontSize: 15,
+  },
+  category: {
     textAlign: 'left',
     margin: 10,
     color: 'black',
+    fontSize: 18,
   },
   searchBtn: {
     width: 150,
@@ -236,24 +322,12 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: 'white',
   },
-  list: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: 'rgb(39, 174, 96)'
-  },
-  row: {
-    margin: 8,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  title: {
-    fontSize: 20,
-    color: 'white'
-  },
   textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1
+    marginLeft: 5,
+    marginRight: 5,
+    fontSize: 15,
+    color: 'black',
+    textAlign: 'center',
   }
 });
 
